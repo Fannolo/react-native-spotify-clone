dc8c264376b13d3722e087be92ddddbe28411f20
alessandro <alessandro.fan@welld.ch>
2022-01-22 17:08:15 +0100

    test: add test to cover title and subtitle component

---
+++ modules/music/playlist/components/typography/Subtitle/Subtitle.test.tsx
@@ -0,0 +1,9 @@
+import { render } from '@testing-library/react-native';
+import Subtitle from '.';
+
+describe('Subtitle', () => {
+  it('should render the component if a text prop is provided', () => {
+    const { getByText } = render(<Subtitle text='Hello' />);
+    expect(getByText('Hello')).not.toBeNull();
+  });
+});
---
+++ modules/music/playlist/components/typography/Title/Title.test.tsx
@@ -0,0 +1,9 @@
+import { render } from '@testing-library/react-native';
+import Title from '.';
+
+describe('Title', () => {
+  it('should render the component if a text prop is provided', () => {
+    const { getByText } = render(<Title text='Hello' />);
+    expect(getByText('Hello')).not.toBeNull();
+  });
+});
