969d9ba4d44d2a21dfb38a033925ae282a992b31
alessandro <alessandro.fan@welld.ch>
2022-01-22 17:25:47 +0100

    feat: add playlist screen to showcase the list of songs

---
+++ modules/music/playlist/screens/PlaylistScreen/PlaylistScreen.test.tsx
@@ -0,0 +1,5 @@
+describe('Playlist screen', () => {
+  it('should render the component as expected');
+  it('should receive from the navigation a playlist id');
+  it('should request a backend call to retrieve the songs of that playlist');
+});
---
+++ modules/music/playlist/screens/PlaylistScreen/PlaylistScreen.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+interface Props {}
+
+const PlaylistScreen = (props: Props) => {
+  return (
+    <View>
+      <Text>Oh Hi</Text>
+    </View>
+  );
+};
+
+export default PlaylistScreen;
