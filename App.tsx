4e3950d9ea494d023dc805284c2adf2fd1cf72bc
alessandro <alessandro.fan@welld.ch>
2022-01-22 17:07:51 +0100

    feat: add safe area view to the app root

--- App.tsx
+++ App.tsx
@@ -1,23 +1,30 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { ApiProvider } from '@reduxjs/toolkit/dist/query/react';
 import React from 'react';
-import { StatusBar } from 'react-native';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { StatusBar, StyleSheet } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 import api from './api';
 import RootStack from './navigation/RootStack';
+import colors from './utils/colors';
 
 const App = () => {
   return (
     <SafeAreaProvider>
-      <StatusBar barStyle={'light-content'} />
-      <ApiProvider api={api}>
-        <NavigationContainer>
-          <RootStack />
-        </NavigationContainer>
-      </ApiProvider>
+      <SafeAreaView style={styles.safeAreaContainer}>
+        <StatusBar barStyle={'light-content'} />
+        <ApiProvider api={api}>
+          <NavigationContainer>
+            <RootStack />
+          </NavigationContainer>
+        </ApiProvider>
+      </SafeAreaView>
     </SafeAreaProvider>
   );
 };
+
+const styles = StyleSheet.create({
+  safeAreaContainer: { flex: 1, backgroundColor: colors.dark.black },
+});
 
 export default App;
