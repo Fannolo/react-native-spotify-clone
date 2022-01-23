3d70def5255accd16d4e831c00a890f6e4aec862
alessandro <alessandro.fan@welld.ch>
2022-01-23 18:49:27 +0100

    fix: remove empty test temporarily

--- api/music/hooks.test.ts
+++
@@ -1,9 +0,0 @@
-import React, { ReactNode } from 'react';
-// import MockAdapter from 'axios-mock-adapter';
-
-describe('music API', () => {
-  describe('playlist', () => {
-    test('useGetFeaturedPlaylists', () => {});
-    test('useGetPlaylistDetail', () => {});
-  });
-});
